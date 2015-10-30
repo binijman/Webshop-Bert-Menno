42c41a08f7512762e2e32cf6ab5783e94443684b
Bert <bertnijman@MacBook-Pro.local>
2015-10-30 16:23:58 +0100

    update

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -69,6 +69,6 @@ class CardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def card_params
-      params.require(:card).permit(:name, :price, :revenue, :owner, :set_size, :group)
+      params.require(:card).permit(:name, :price, :revenue, :owner, :set_size, :group, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030144318) do
+ActiveRecord::Schema.define(version: 20151030145509) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151030144318) do
     t.string   "group"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
